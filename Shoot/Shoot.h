f31d91c044040b23c26fd3b0c144522f9b506bd5
mohamedmahmoud6196 <33757410+mohamedmahmoud6196@users.noreply.github.com>
2023-01-12 11:41:39 +0200

    Initial Commit

---
+++ Shoot/Shoot.h
@@ -0,0 +1,18 @@
+//
+//  Shoot.h
+//  Shoot
+//
+//  Created by Mimo on 12/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shoot.
+FOUNDATION_EXPORT double ShootVersionNumber;
+
+//! Project version string for Shoot.
+FOUNDATION_EXPORT const unsigned char ShootVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shoot/PublicHeader.h>
+
+
